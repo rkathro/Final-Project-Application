51287fa9932fbbc8153a5fd7494048ff96428d4e
Bradenm316 <135643695+Bradenm316@users.noreply.github.com>
2024-03-20 21:36:45 -0400

    Changed every file to fit our application

--- app/src/main/java/com/example/myapplication/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/account/AccountFragment.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.ui.home
+package com.example.myapplication.ui.account
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,29 +7,30 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.myapplication.databinding.FragmentHomeBinding
+import com.example.myapplication.databinding.FragmentAccountBinding
 
-class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+class AccountFragment : Fragment() {
+
+    private var _binding: FragmentAccountBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+        val accountViewModel =
+            ViewModelProvider(this).get(AccountViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        accountViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
