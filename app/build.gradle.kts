1a208f79f1fa9152e2a9a32419e75cf7946560ce
Bradenm316 <Bradenm316@gmail.com>
2024-05-01 13:37:31 -0400

    implementing mongodb

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
-    id("com.android.application")
-    kotlin("android")
-    kotlin("kapt")
-    id("io.realm.kotlin")
 }
 
 android {
@@ -17,6 +13,7 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -38,6 +35,7 @@ android {
         viewBinding = true
         dataBinding = true
     }
+
 }
 
 dependencies {
@@ -51,10 +49,11 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.library)
-    implementation("io.realm.kotlin:library-base:1.15.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("org.mongodb:mongodb-driver-sync:4.4.3")
+    implementation("com.android.support:multidex:1.0.3")
 
     val camerax_version = "1.4.0-alpha05"
     implementation("androidx.camera:camera-core:$camerax_version")
@@ -62,10 +61,5 @@ dependencies {
     implementation ("androidx.camera:camera-lifecycle:$camerax_version")
     implementation("androidx.camera:camera-video:${camerax_version}")
     implementation("androidx.camera:camera-view:${camerax_version}")
-}
 
-kotlin {
-    sourceSets["main"].kotlin.srcDirs("build/generated/ksp/main/kotlin")
 }
-
-apply(plugin = "io.realm.kotlin")
