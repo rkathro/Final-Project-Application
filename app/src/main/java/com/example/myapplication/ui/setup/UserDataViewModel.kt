59cd313a71a14ad726ea8a8b88a7436ac6c577bd
Bradenm316 <Bradenm316@gmail.com>
2024-05-01 15:26:49 -0400

    added getters and setters

--- app/src/main/java/com/example/myapplication/ui/setup/UserDataViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/setup/UserDataViewModel.kt
@@ -13,8 +13,38 @@ class UserDataViewModel : ViewModel() {
     fun setUserData(username: String, password: String, email: String, phoneNumber: String) {
         _userData.value = UserData(username, password, email, phoneNumber)
     }
-
     fun getUserData(): UserData? {
         return _userData.value
+    }
+    fun setUsername(username: String) {
+        _userData.value = _userData.value?.copy(username = username)
+    }
+
+    fun setPasswrod(password: String) {
+        _userData.value = _userData.value?.copy(password = password)
+    }
+
+    fun setEmail(email: String) {
+        _userData.value = _userData.value?.copy(email = email)
+    }
+
+    fun setPhoneNumber(phoneNumber: String) {
+        _userData.value = _userData.value?.copy(phoneNumber = phoneNumber)
+    }
+
+    fun getUsername(): String? {
+        return _userData.value?.username
+    }
+
+    fun getPassword(): String? {
+        return _userData.value?.password
+    }
+
+    fun getEmail(): String? {
+        return _userData.value?.email
+    }
+
+    fun getPhoneNumber(): String? {
+        return _userData.value?.phoneNumber
     }
 }
